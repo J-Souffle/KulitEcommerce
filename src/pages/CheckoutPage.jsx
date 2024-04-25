216ff20fa7ae5a8678b5e25d371c22a9e5978017
Joshua Cancio <jd.cancio1@gmail.com>
2024-04-25 19:26:16 -0400

    Added confirmation for successful/failed pay

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -7,11 +7,30 @@ import ShippingInfo from "../components/CheckoutComponents/ShippingInfo.jsx";
 import PaymentDetails from "../components/CheckoutComponents/PaymentDetails.jsx";
 import ConfirmationButton from "../components/CheckoutComponents/ConfirmationButton.jsx";
 import Footer from "../components/Footer.jsx";
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
+
+const MySwal = withReactContent(Swal);
 
 function Checkout() {
   const publishableKey = 'pk_test_51P7ai8LO5J7ORzPKB8mr7QaPvwECu3ebmWth80FNICCRX6ehA62vlkqUNwskIb678eCsIxmNNMPOVsL7sbv3M8CP00TFgHUti4'
   const [product, setProduct] = useState({name: 'Headphone', price: 5});
   const priceForStripe = product.price * 100;
+
+  const handleSuccess =() => {
+    MySwal.fire({
+      icon: 'success',
+      title: 'Payment was successful',
+      time: 4000,
+    });
+  }
+  const handleFailure =() => {
+    MySwal.fire({
+      icon: 'error',
+      title: 'Payment was not successful',
+      time: 4000,
+    });
+  }
   const payNow = async token => {
     try {
       const response = await axios({
@@ -23,9 +42,11 @@ function Checkout() {
         }
       });
       if(response.status === 200) {
+        handleSuccess();
         console.log('Your payment was successful');
       }
     } catch (error) {
+      handleFailure();
       console.log(error);
     }
   }
