ddfa71cc8a150e6464a37efbf21f651a789c3f7c
Joshua Cancio <145736028+J-Souffle@users.noreply.github.com>
2024-09-12 19:19:32 -0400

    Updated Images

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,9 +3,9 @@ import Main1 from "../img/header/AllClothing.jpg";
 import Main2 from "../img/header/Hoodies.jpg";
 import Main3 from "../img/header/Shirts.jpg";
 import Main4 from "../img/header/Other.jpg";
-// import Main5 from "../img/header/OtherSticker.jpeg"
-// import Main6 from "../img/header/Socks.jpeg"
-// import Main7 from "../img/header/PhilippinesSweatshirt.jpeg"
+import Main5 from "../img/header/OtherSticker.jpeg"
+import Main6 from "../img/header/Accessories.jpeg"
+import Main7 from "../img/header/Tops.jpeg"
 import { Link } from "react-router-dom";
 
 function Hero() {
@@ -24,21 +24,21 @@ function Hero() {
             <div className="featured grid-two">
               <Link to="categories/tops">
                 <div id="img2" className="lil-overlay"></div>
-                <img src={Main2} alt="img2" />
+                <img src={Main7} alt="img2" />
                 <p className="main-description">Tops</p>
               </Link>
             </div>
             <div className="featured grid-four">
               <Link to="categories/accessories">
                 <div id="img3" className="lil-overlay"></div>
-                <img src={Main3} alt="img3" />
+                <img src={Main6} alt="img3" />
                 <p className="main-description">Accessories</p>
               </Link>
             </div>
             <div className="featured grid-four-low">
               <Link to="categories/other">
                 <div id="img4" className="lil-overlay"></div>
-                <img src={Main4} alt="img4" />
+                <img src={Main5} alt="img4" />
                 <p className="main-description">Other</p>
               </Link>
             </div>
